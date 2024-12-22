18a12a120e7de8bbffc7f7cdb1b19138e6fa3449
Bulker61 <bulker61@gmail.com>
2024-12-22 21:13:27 +0400

    Add files via upload

--- module_1_final.py
+++ module_1_final.py
@@ -1,6 +1,7 @@
-sorted_students = ['Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve']
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sorted(students)
 average_grades = {}
-for name, grade_list in zip(sorted_students, grades):
+for name, grade_list in zip(students, grades):
      average_grades[name] = sum(grade_list) / len(grade_list)
 print(average_grades)
